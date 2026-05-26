d59212430385a50f294c2ed7351beedbbd8db925
2nayeon <leenayeon0915@gmail.com>
2026-05-26 12:20:41 +0900

    Chore: 초기 프로젝트 구조 세팅

---
+++ src/main/java/com/routee/backend/RouteeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.routee.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/routee/backend/RouteeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routee.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
