a71fddd7f66230d183fac618b2a8b928400c31af
2nayeon <leenayeon0915@gmail.com>
2026-05-26 20:24:19 +0900

    chore: 폴더 구조 변경

--- src/main/java/com/routee/RouteeBackendApplication.java
+++ src/main/java/com/routee/RouteeBackendApplication.java
@@ -1,4 +1,4 @@
-package com.routee.backend;
+package com.routee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/routee/RouteeBackendApplicationTests.java
+++ src/test/java/com/routee/RouteeBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.routee.backend;
+package com.routee;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
